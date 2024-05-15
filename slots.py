9f046e6ca601edfacc2739842560e1294c030fcc
chettriyuvraj <chettriyuvraj@gmail.com>
2024-05-15 13:02:03 +0100

    feat: change email alert to sound alert - connect raspi to HDMI and sound alert will play

--- slots.py
+++ slots.py
@@ -1,35 +1,15 @@
-def send_email(msg):
-    from dotenv import load_dotenv
-    import os
-    import requests
-    import json
-    load_dotenv()
-
-    url = os.getenv("API_URL")
-    email_to = os.getenv("EMAIL_TO")
-    api_key=os.getenv("API_KEY")
-
-    payload = json.dumps(
-        {
-            "sender": {"name": "Visa Alerts", "email": "alerts@alerts.com"},
-            "to": [{"email": f"{email_to}"}],
-            "subject": "Visa Alert",
-            "textContent": msg,
-        }
-    )
-    headers = {
-        "accept": "application/json",
-        "api-key": api_key,
-        "content-type": "application/json",
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
-
 def get_visa_slots_html():
     from selenium import webdriver
     # Set up Selenium WebDriver (make sure to have the appropriate driver for your browser installed)
     # Here, I'm using Chrome as an example. You can use Firefox, Edge, etc., by changing the driver accordingly.
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    #options.binary_location ='/usr/bin/chromedriver'
+    options.add_argument('--headless')
+    service = webdriver.ChromeService(executable_path = '/usr/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=options)
+
+
+    # driver = webdriver.Chrome('usr/lib/chromium-browser/chromedriver')
 
     # URL to visit
     url = "https://visaslots.info/"
@@ -61,6 +41,7 @@ def fetch_earliest_slots_td_tags(html_doc):
 
 def start_visa_alerts():
     import time
+    import pygame
     while True: 
         visa_slots_html = get_visa_slots_html()
         td_tags = fetch_earliest_slots_td_tags(visa_slots_html)
@@ -68,16 +49,22 @@ def start_visa_alerts():
 
         for td_tag in td_tags:
             text = td_tag.text
-            if "2024" in text and ("May" in text or "Jun" in text or "Jul" in text or "Sep" in text):
+            if "2024" in text and ("May" in text or "Jun" in text or "Jul" in text):
                 for sibling in td_tag.next_siblings:
                     if not sibling.text.isspace():
                         valid_slots.append(text + "! Count: " + sibling.text + "\n\n")
                         break
 
-        if len(valid_slots) > 0:
+        if len(valid_slots) >= 0:
+            print("Valid slots found...")
             msg = "The slots are" + "\n".join(valid_slots)
-            send_email(msg)
-        time.sleep(500)
+            pygame.init()
+            sound = pygame.mixer.Sound('./alert')
+            sound.play(loops=3)
+        else:
+            print("Valid slots not found...")
+        print("Sleeping...")
+        time.sleep(90)
 
 
 start_visa_alerts()
